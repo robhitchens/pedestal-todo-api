2861cf3da4fccbba5d3e5dc8718fe6cb3ef20c3a
Robert Hitchens <rjhitchens3@gmail.com>
2020-08-30 20:22:11 -0400

    - added more stuff to the apis

--- src/main.clj
+++ src/main.clj
@@ -66,13 +66,55 @@
          context)
        context))})
 
+(def entity-render 
+  {:name :entity-render
+   :leave
+   (fn [context]
+     (if-let [item (:result context)]
+       (assoc context :response (ok item))
+       context))})
+
+(defn find-list-item-by-ids [dbval list-id item-id]
+  (get-in dbval [list-id :items item-id] nil))
+
+(def list-item-view
+  {:name :list-item-view
+   :leave
+   (fn [context]
+     (if-let [list-id (get-in context [:request :path-params :list-id])]
+       (if-let [item-id (get-in context [:request :path-params :item-id])]
+         (if-let [item (find-list-item-by-ids (get-in context [:request :database]) list-id item-id)]
+           (assoc context :result item)
+           context)
+         context)
+       context))})
+
+(defn list-item-add
+  [dbval list-id item-id new-item]
+  (if (contains? dbval list-id)
+    (assoc-in dbval [list-id :items item-id] new-item)
+    dbval))
+
+(def list-item-create
+  {:name :list-item-view
+   :enter
+   (fn [context]
+     (if-let [list-id (get-in context [:request :path-params :list-id])]
+       (let [nm       (get-in context [:request :query-params :name] "Unnamed Item")
+             new-item (make-list-item nm)
+             item-id  (str (gensym "i"))]
+         (-> context
+             (assoc :tx-data [list-item-add list-id item-id new-item])
+             (assoc-in [:request :path-params :item-id] item-id)))
+       context))})
+
 (def routes
   (route/expand-routes
    #{["/todo"                   :post   [db-interceptor list-create]]
      ["/todo"                   :get    echo :route-name :list-query-form]
-     ["/todo/:list-id"          :get    echo :route-name :list-view]
-     ["/todo/:list-id"          :post   echo :route-name :list-item-create]
-     ["/todo/:list-id/:item-id" :get    echo :route-name :list-item-view]
+     ["/todo/:list-id"          :get    [entity-render db-interceptor list-view]]
+     ["/todo/:list-id"          :post   [entity-render list-item-view db-interceptor list-item-create]]
+     ["/todo/:list-id/:item-id" :get    [entity-render list-item-view db-interceptor]]
      ["/todo/:list-id/:item-id" :put    echo :route-name :list-item-update]
      ["/todo/:list-id/:item-id" :delete echo :route-name :list-item-delete]}))
 
